a83f3325a2430f10e8678765955f94884faf18fe
cdpatelgit <chiragpatel.mca20@gmail.com>
2016-07-23 15:34:58 +0530

    Add files via upload

--- src/main/java/com/db/ss/controller/Application.java
+++ src/main/java/com/db/ss/controller/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
+@ComponentScan(basePackages="com.db")
 @EnableAutoConfiguration
 public class Application {
 
